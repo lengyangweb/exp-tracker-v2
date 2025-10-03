979ce11f6d7aa9f142e5d5b6d976479914758f2a
Leng Yang <lengyang.web@gmail.com>
2025-10-03 16:05:25 -0500

    add sidebar

--- src/app/page.js
+++ src/app/page.js
@@ -1,12 +1,20 @@
+import { AppSidebar } from '@/components/shared/app-sidebar'
 import { Button } from '@/components/ui/button'
 import { Card, CardContent, CardTitle } from '@/components/ui/card'
 import { Input } from '@/components/ui/input'
 import { Label } from '@/components/ui/label'
+import { SidebarProvider } from '@/components/ui/sidebar'
+import { Spinner } from '@/components/ui/spinner'
 import React from 'react'
 
 const page = () => {
   return (
-    <h1>Hello World!</h1>
+    <>
+      <SidebarProvider>
+        <AppSidebar />
+        {/* <h1>Hello World!</h1> */}
+      </SidebarProvider>
+    </>
   )
 }
 
